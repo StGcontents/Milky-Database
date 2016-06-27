6ca7fb1ff1eeb9323b51f41d977165c86e755513
Emanuele_Della_Camera <dellacameraemanuele@gmail.com>
2016-06-27 18:41:53 +0200

    LoginController,SerializzatoreUtente,BoundaryUtenteLogin,BoundaryUtenteMEnu

--- Milky DataBase/src/controller/LoginController.java
+++ Milky DataBase/src/controller/LoginController.java
@@ -1,23 +1,37 @@
 package controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
+import Serializzatore.SerializzatoreUtente;
 import model.User;
-import modelBean.UserBean;
 
-public class LoginController {
-	
-	public boolean login (UserBean u)
+public class LoginController
+{
+
+	public boolean login (String userName, String password)
 	{
 		HashMap<String, User> utenti = SerializzatoreUtente.getUtenti();
 		
-		User utenteCercato = utenti.get(u.getUserId());
-		if (utenteCercato == null)
-			return false;
-
-		if (utenteCercato.getPassword().equals(u.getPassword()))
-			return true;
+		List<User> utentiUserNameList = new ArrayList<User>();
 		
+		for (String id : utenti.keySet()) {
+			utentiUserNameList.add(SerializzatoreUtente.getUtenteByUserName(userName));
+		}
+		
+		for(User u : utentiUserNameList){
+			if(u != null){
+				if(u.getPassword().equals(password)){
+					return true;
+				}
+			}
+		}
 		return false;
 	}
+	
+	public User getUtente(String userName){
+		return (SerializzatoreUtente.getUtenteByUserName(userName));
+	}
+
 }
